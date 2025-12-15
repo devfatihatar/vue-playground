12fc89bd2be799a2930fb08455bf98411f2b22e1
Fatih Atar <fatihatardevv@outlook.com>
2025-12-16 00:50:51 +0300

    feat: init vuee playground

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+  import RefDemo from './exercises/01-ref/RefDemo.vue';
+</script>
+
+<template>
+  <RefDemo />
+</template>
---
+++ src/exercises/01-ref/RefDemo.vue
@@ -0,0 +1,35 @@
+<script setup>
+// ref: tek bir değeri reaktif hale getirir (reactive = değişince UI güncellenir)
+import { computed } from "@vue/reactivity"
+import { ref } from "vue"
+
+const count = ref(0)  //count.value = gerçek değer
+const name = ref("Fatih") //name değişkenini reactive hale getirdik.
+
+//count değişkenindeki sayıyı artıran fonksiyon.
+function increment(){
+    count.value++
+}
+
+const upperName = computed(() => {
+    return name.value.toUpperCase()
+})
+</script>
+
+<template>
+    <div style="padding:16px">
+        <h2>01 - ref demo</h2>
+
+        <p><b>Count:</b> {{  count  }}</p>
+        <button @click="increment">Increase</button>
+        
+        <hr />
+
+        <label>
+            <b>name:</b>
+            <input v-model="name" placeholder="Type your name">
+        </label>
+        <p>Hello, {{ name }}</p>
+        <p>Uppercase: {{ upperName }}</p>
+    </div>
+</template>
